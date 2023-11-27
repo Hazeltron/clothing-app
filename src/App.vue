73493f8bdc0753878d4cdd2ba717ce4dd35f3746
Hazeltron <paulhazelton.work@gmail.com>
2023-11-27 15:57:51 -0700

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,15 @@
 
 <template>
 	<header>
-		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+		
 
 		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
+			<HelloWorld msg="Eyre Apparel" />
 
 			<nav>
 				<RouterLink to="/">Home</RouterLink>
 				<RouterLink to="/about">About</RouterLink>
+				<RouterLink to="/store">Store</RouterLink>
 			</nav>
 		</div>
 	</header>
