f716a17277d246e17ebbd9f8a5a145b706aa7da8
Hazeltron <paulhazelton.work@gmail.com>
2023-11-27 18:57:24 -0700

    Initialized store for storeview

---
+++ src/stores/stored-items.js
@@ -0,0 +1,13 @@
+import { ref, computed, reactive } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useItemsStore = defineStore('items', function() {
+    const list = reactive([
+        {item: "t-shirt", price: 200},
+    ]);
+    function add(item){
+        list.push(item);
+    }
+
+  return {list, add};
+});
