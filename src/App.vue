4f48aa7af87f9206bcfe2d351a8dfa7137c40b29
Hazeltron <paulhazelton.work@gmail.com>
2023-11-30 16:15:53 -0700

    Fixed svg sprite

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,88 @@
 <script setup>
-	import { RouterLink, RouterView, useRoute } from 'vue-router';
-	import { useInterfaceStore } from '@/stores/interface';
+import { RouterLink, RouterView, useRoute } from "vue-router";
+import { useInterfaceStore } from "@/stores/interface";
 
-	const ui = useInterfaceStore();
-	const route = useRoute();
+const ui = useInterfaceStore();
+const route = useRoute();
 </script>
 
+
+
 <template>
-	<header :class="`${route.name} ${ui.menuClass}`" class="site-header" >
-		<inner-column>
+  <header :class="`${route.name} ${ui.menuClass}`" class="site-header">
+    <inner-column>
+      <div class="wrapper">
+        <nav>
+          <RouterLink class="links" @click="ui.mainMenuClose" to="/"
+            >Home</RouterLink
+          >
+          <RouterLink class="links" @click="ui.mainMenuClose" to="/about"
+            >About</RouterLink
+          >
+          <RouterLink class="links" @click="ui.mainMenuClose" to="/store"
+            >Store</RouterLink
+          >
 
-			<div class="wrapper">
-		
-				<nav >
-					<RouterLink class="links" @click="ui.mainMenuClose" to="/">Home</RouterLink>
-					<RouterLink class="links" @click="ui.mainMenuClose" to="/about">About</RouterLink>
-					<RouterLink class="links" @click="ui.mainMenuClose" to="/store">Store</RouterLink>
+          <button class="toggle" @click="ui.toggleMenu">Close</button>
+        </nav>
 
-					<button class="close toggle" @click="ui.toggleMenu">Close</button>
-				</nav>
+        <button class="toggle" @click="ui.toggleMenu"><svg class="icon-menu"><use xlink:href="#icon-menu"></use></svg> </button>
+      </div>
+    </inner-column>
+  </header>
 
-				<button class="open toggle" @click="ui.toggleMenu">Menu</button>
-
-			</div>
-
-		</inner-column>
-	</header>
-
-	<main>
-		<inner-column>
-			<RouterView />
-		</inner-column>
-		
-	</main>
-
-	
+  <main>
+    <inner-column>
+      <RouterView />
+    </inner-column>
+  </main>
 </template>
 
 <style scoped>
 .site-header {
-	background-color: var(--paper);
+  background-color: var(--paper);
 }
 nav {
-	display: flex;
-	flex-direction: row;
-	gap: 2rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
 }
 
 .menu-closed nav {
-	display: none;
+  display: none;
 }
-
-
+.icon-menu {
+	color: var(--shadow);
+}
 .menu-open nav {
-	display: flex;
-	position: fixed;
-	top: 0;
-	left: 0;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	width: 100%;
-	height: 100vh;
-	background-color: var(--shadow);
-
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background-color: var(--shadow);
 }
 
-@media (min-width: 600px) {
-	  .menu-closed nav {
-		display: flex;
-		flex: row;
-	}
+.toggle {
+	display: block;
+}
 
-	.menu-open nav {
-		display: flex;
-		flex: row;
-	}
-	.toggle {
-		display: none;
-		
-	}
+@media (min-width: 800px) {
+  .menu-closed nav {
+    display: flex;
+    flex: row;
+  }
 
+  .menu-open nav {
+    display: flex;
+    flex: row;
+  }
+  .toggle {
+    display: none;
+  }
 }
 </style>
