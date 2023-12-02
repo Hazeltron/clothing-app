f49c08787504f9ba61b59eeee4b27cc1ffabcc20
Hazeltron <paulhazelton.work@gmail.com>
2023-12-02 14:55:49 -0700

    Updated item form to reflect travel app

--- src/components/ItemForm.vue
+++ src/components/ItemForm.vue
@@ -28,25 +28,27 @@ function clear(){
 
 <template>
   <div class="store">
-    <h1 class="attention-voice">This is a store page</h1>
+    
 
     <form @submit.prevent="save">
+      <h1 class="attention-voice">A World of Possibilities</h1>
       <div class="field">
-        <label for="item" class="calm-voice">Add item:</label>
-        <input type="text" id="item" v-model="items.item" />
+        <label for="item" class="calm-voice">Where would you like to go?</label>
+        <input required type="text" id="item" v-model="items.item" />
       </div>
 
       <div class="field">
-        <label for="price" class="calm-voice">Add price:</label>
-        <input type="number" id="price" v-model="items.price" />
+        <label for="price" class="calm-voice">What's your estimated budget?</label>
+        <input required type="number" id="price" v-model="items.price" />
       </div>
 
-      <button type="submit" class="add calm-voice">Add Item:</button>
+      <button type="submit" class="add calm-voice">Create <br> Suitcase <svg class="icon-twitter"><use xlink:href="#icon-suitcase"></use></svg></button>
     </form>
 
-    <ul>
+    <ul class="suitcase-list">
+      <li class="attention-voice">Suitcases</li>
         <li v-for="items in store.list">
-          <RouterLink :to="`item/${items.slug}`">
+          <RouterLink class="calm-voice suitcases" :to="`item/${items.slug}`">
             {{ items.item}} ${{ items.price }}
           </RouterLink>  
            
@@ -55,15 +57,25 @@ function clear(){
   </div>
 </template>
 
-<style>
+<style scoped>
 
 body {
   background-color: var(--shadow);
 }
+
 .store {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 20%;
+}
+
+form {
   display: grid;
   gap: 30px;
 }
+
+
 
 form {
   display: grid;
@@ -82,10 +94,33 @@ input {
 input:focus {
   outline-color: var(--highlight);
 }
+
 .add {
+  display: flex;
+  flex-direction: row;
+  align-items: center ;
+  justify-content: space-between;
   max-width: 150px;
   background-color: var(--white);
   border-radius: 20px 20px;
-  padding: 0 20px;
+  padding: 10px;
+  border: solid 1px var(--shadow);
+}
+
+.add:hover {
+  
+  background-color: var(--shadow);
+  color:  var(--white);
+}
+
+a.suitcases {
+  padding: 10px;
+  border: 1px solid var(--shadow);
+  border-radius: 10px;
+}
+
+.suitcase-list {
+  display: grid;
+  gap: 10px;
 }
 </style>
