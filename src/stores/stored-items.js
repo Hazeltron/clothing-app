4de1843cbd741fbb9d6251e7a0c703ea7f975f9b
Hazeltron <paulhazelton.work@gmail.com>
2023-12-04 22:08:55 -0700

    Added spereate functions and arrays for each option in details page and added computed budget that is broken atm

--- src/stores/stored-items.js
+++ src/stores/stored-items.js
@@ -1,12 +1,35 @@
-import { reactive } from 'vue'
+import { reactive, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useItemsStore = defineStore('items', function() {
+
+    // const budget = function budget(){
+    //     const budget = list.price - (detail.locationPrice + activities.activityPrice);
+    //     return budget
+
+    // }
+
+    const costs = computed(function () {
+        const listTotalPrice = list.reduce(function (total, item) {
+          return total + (item.price || 0);
+        }, 0);
+      
+        const locationTotalPrice = detail.reduce(function (total, location) {
+          return total + (location.locationPrice || 0);
+        }, 0);
+      
+        const activityTotalPrice = activities.reduce(function (total, activity) {
+          return total + (activity.activityPrice || 0);
+        }, 0);
+      
+        return listTotalPrice - locationTotalPrice - activityTotalPrice;
+      });
+
     const list = reactive([
         {
         item: "Disney World", 
         price: 4000,
-        slug: "Disney World"
+        slug: "Disney Word"
     }
     ]);
 
@@ -17,6 +40,21 @@ export const useItemsStore = defineStore('items', function() {
     }
     ]);
 
+    const activities = reactive([
+        {
+            activity: "",
+            activityPrice: ""
+        }
+
+    ]);
+
+    const notes = reactive([
+        {
+            note: ""
+        }
+
+    ]);
+
     function add(item){
         list.push(item);
     }
@@ -25,5 +63,24 @@ export const useItemsStore = defineStore('items', function() {
         detail.push(details);
     }
 
-  return {list, detail, add, addDetails };
+    function addActivity(activity){
+        activities.push(activity);
+    }
+
+    function addNote(note){
+        notes.push(note);
+    }
+
+    function deleteNote(index) {
+        notes.splice(index, 1);
+    }
+
+    function deleteDetail(index) {
+        detail.splice(index, 1);
+    }
+    
+    function deleteActivity(index) {
+        activities.splice(index, 1);
+    }
+  return {list, detail, notes, activities, add, addDetails, addActivity, deleteDetail, deleteActivity, addNote, deleteNote, costs };
 });
