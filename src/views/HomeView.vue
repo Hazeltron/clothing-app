83ea2547610af07993d1c3a573d9982f86492f4d
Hazeltron <paulhazelton.work@gmail.com>
2023-12-12 21:32:51 -0700

    Added map and updated homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,29 @@ import ItemForm from "../components/ItemForm.vue";
 </script>
 
 <template>
-    <inner-column>
-      <ItemForm />
-    </inner-column>
     
+      <section>
+        <inner-column>
+          <h1 class="loud-voice">Peregrine</h1>
+        <p class="attention-voice">Travel Anywhere</p>
+        </inner-column>
+      </section>
+      
+      <section>
+        <inner-column>
+          <ItemForm />
+        </inner-column>
+      </section>
+      
+
+      <section>
+        <inner-column>
+          <div id="map" style="height: 400px;"></div>
+        </inner-column>
+      </section>
+      
+
+
     
  
 </template>
